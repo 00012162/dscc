6dd03d732d1fd3e6f8f350de7f5d0e508f35430c
00012162 <wiut00012162@gmail.com>
2023-11-07 18:55:53 +0500

    feat: last commit

---
+++ Backend/Backend/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Data
+{
+    public class DataContext : DbContext { 
+        public DataContext(DbContextOptions<DataContext> options) : base(options) {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Writer> Writers => Set<Writer>();
+        public DbSet<Book> Books => Set<Book>();
+    }
+}
---
+++ Backend/Backend/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+namespace Backend.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        IWriterRepository WriterRepository { get; }
+        IBookRepository BookRepository { get; }
+        Task<bool> SaveAsync();
+    }
+}
